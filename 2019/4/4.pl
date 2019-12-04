6155066913cc874f5f04bd31bc6e2f1183118add
Dionysis Zindros <dionyziz@gmail.com>
2019-12-05 01:28:13 +0200

    Solve Day 4-2 problem in Prolog

--- 2019/4/4.pl
+++ 2019/4/4.pl
@@ -7,12 +7,27 @@ incr([_]).
 incr([H1, H2|T]) :-
   H1 =< H2, incr([H2|T]).
 
-good(X) :-
-  string_codes(X, Y), repeat(Y), incr(Y).
+good(Z, 1) :-
+  repeat(Z), incr(Z).
+good(Z, 2) :-
+  clearRepeat(Z, 0), incr(Z).
 
-candidate(X) :-
-  between(356261, 846303, X), number_string(X, Y), good(Y).
+clearRepeat([H1, H2, H3|_], 0) :-
+  H1 = H2, H2 \= H3.
+clearRepeat([H1, H2, H3, H4|_], _) :-
+  H1 \= H2, H2 = H3, H3 \= H4.
+clearRepeat([H1, H2, H3], _) :-
+  H1 \= H2, H2 = H3.
+clearRepeat([_|T], I) :-
+  clearRepeat(T, I + 1).
+
+candidate(X, Question) :-
+  between(356261, 846303, X),
+  number_string(X, Y), string_codes(Y, Z),
+  good(Z, Question).
 
 main :-
-  aggregate(count, X, candidate(X), N),
-  write(N).
+  aggregate(count, X, candidate(X, 1), N1),
+  aggregate(count, X, candidate(X, 2), N2),
+  writeln(N1),
+  writeln(N2).
