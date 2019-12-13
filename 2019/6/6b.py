c53d57af79d997c7dd2c9027dea4b700fcd5bcf3
Dionysis Zindros <dionyziz@gmail.com>
2019-12-13 21:46:01 +0200

    Further improve solution to Day 6

--- 2019/6/6b.py
+++ 2019/6/6b.py
@@ -3,13 +3,13 @@ from collections import defaultdict
 with open('6.txt') as f:
     contents = f.read()
 
-adj = defaultdict(lambda: [])
+adj = defaultdict(lambda: set())
 
 for line in contents.split("\n"):
     if line != "":
         u, v = line.split(")")
-        adj[u].append(v)
-        adj[v].append(u)
+        adj[u].add(v)
+        adj[v].add(u)
 
 def bfs(s, t):
     visited = set()
@@ -19,9 +19,7 @@ def bfs(s, t):
         visited.add(v)
         if v == t:
             return c
-        if v in adj:
-            for w in adj[v]:
-                if w not in visited:
-                    q.append((w, c + 1))
+        for w in adj[v].difference(visited):
+            q.append((w, c + 1))
 
 print(bfs("YOU", "SAN") - 2)
