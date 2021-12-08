7bd1a84e49007c555dccc51bee2432e02719ad69
Dionysis Zindros <dionyziz@gmail.com>
2021-12-07 20:46:59 -0800

    Closed-form solution for 7a

--- 2021/7/7.py
+++ 2021/7/7.py
@@ -1,11 +1,3 @@
 with open('in.txt') as f:
-  crabs = list(map(int, f.read().split(',')))
-
-best = float('inf')
-for x in range(min(crabs), max(crabs) + 1):
-  cost = 0
-  for crab in crabs:
-    cost += abs(crab - x)
-  best = min(cost, best)
-
-print(best)
+  crabs = sorted(map(int, f.read().split(',')))
+print(sum([abs(crab - crabs[len(crabs) // 2]) for crab in crabs]))
