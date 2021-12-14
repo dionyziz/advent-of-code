7717791114f93af6a698075b556b114d997f980d
Dionysis Zindros <dionyziz@gmail.com>
2021-12-13 22:02:51 -0800

    Minor refactor in 2021 day 3

--- 2021/3/3.py
+++ 2021/3/3.py
@@ -6,8 +6,8 @@ with open('in.txt') as f:
 transposed = zip(*numbers)
 
 bitcounts = map(Counter, transposed)
-gammabits = list(map(lambda x: x.most_common(1)[0][0], bitcounts))
-epsilonbits = map(lambda x: '0' if x == '1' else '1', gammabits)
+gammabits = [x.most_common(1)[0][0] for x in bitcounts]
+epsilonbits = ['0' if x == '1' else '1' for x in gammabits]
 
 gamma = int(''.join(gammabits), 2)
 epsilon = int(''.join(epsilonbits), 2)
