ae32b66a50271ead8112517c8a9d960bf69aee13
Dionysis Zindros <dionyziz@gmail.com>
2021-12-13 21:17:00 -0800

    Solve 2021 Day 14

---
+++ 2021/14/14.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+with open('in.txt') as f:
+  template, rules = f.read().split('\n\n')
+
+rules = rules[:-1]
+
+def step(str):
+  ret = ''
+  for i in range(len(str) - 1):
+    lookup = str[i:i + 2]
+    ret += str[i] + apply[lookup]
+  ret += str[-1]
+  return ret
+
+apply = {}
+rules = rules.split('\n')
+for rule in rules:
+  left, right = rule.split(' -> ')
+  apply[left] = right
+
+for i in range(10):
+  template = step(template)
+
+z = Counter(template)
+x = z.most_common()
+print(x[0][1] - x[-1][1])
